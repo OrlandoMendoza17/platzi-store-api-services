673988a39cb7ba1f7ebf4d6d7b42edd9e40721d9
OrlandoMendoza17 <ommv.17@gmail.com>
2022-06-14 03:30:04 -0400

    Se provaron todos los request de product.service

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,41 @@ import ProductsService from "./services/product.service";
 
 (async () => {
   const productsAPI = new ProductsService();
+  try {
+    const products = await productsAPI.getAll({ limit: 10, offset: 0 });
+    console.log(products.map((product) => product.title));
 
-  const products = await productsAPI.getAll({ limit: 10, offset: 0 });
+    // const newProduct = await productsAPI.create({
+    //   title: "Nuevo producto de Orlando",
+    //   categoryId: 3,
+    //   price: 1000,
+    //   description: "Producto hecho desde practica de typescript",
+    //   images: [""]
+    // })
+    // console.log("newProduct:", newProduct)
 
-  console.log(products.map((product) => product.title));
+    const userId = 100;
+
+    // const updatedProduct = await productsAPI.update(userId, {
+    //   title: "Producto actualizado de Orlando",
+    //   categoryId: 5,
+    //   price: 1000,
+    //   description: "Producto hecho desde practica de typescript, actualizado",
+    //   images: [
+    //     "https://upload.wikimedia.org/wikipedia/commons/3/32/Platzi.jpg",
+    //   ],
+    // });
+
+    // console.log("updatedProduct:", updatedProduct);
+    
+    // await productsAPI.delete(userId)
+    
+    // console.log(`Producto ${userId} eliminado`)
+    
+    const foundProduct = await productsAPI.getByID(userId)
+    console.log("foundProduct:", foundProduct)
+    
+  } catch (error: any) {
+    console.log(error, error?.message)
+  }
 })();
